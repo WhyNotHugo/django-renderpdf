1ad0a5f4b4d18ff79ff2315e02f30de52c5d8894
Hugo Osvaldo Barrera <hugo@barrera.io>
2017-10-14 18:27:41 -0300

    Python3.4 and PyPy won't work, don't even bother
    PRs welcome

--- setup.py
+++ setup.py
@@ -30,7 +30,6 @@ setup(
         'License :: OSI Approved :: ISC License (ISCL)',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Topic :: Internet :: WWW/HTTP',
