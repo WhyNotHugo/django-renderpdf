74561731ceed5a89de2af08fc09510ae33ad6ac4
Hugo Osvaldo Barrera <hugo@barrera.io>
2017-10-14 17:02:20 -0300

    Initial test scaffold (and just one test)

---
+++ testapp/testapp/testmain/tests/test_views.py
@@ -0,0 +1,30 @@
+from django.test import RequestFactory, TestCase
+
+from testapp.testmain import views
+
+factory = RequestFactory()
+
+
+class PromptDownloadTestCase(TestCase):
+
+    def test_prompt_download(self):
+        request = factory.get('/some_view')
+
+        response = views.PromptDownloadView.as_view()(request)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(
+            b'Content-Type: application/pdf',
+            response.serialize_headers().splitlines()
+        )
+
+
+class ForceDownloadTestCase(TestCase):
+    pass
+
+
+class CustomUrlFetcherTestCase(TestCase):
+    pass
+
+
+class StaticFileResolutionTestCase(TestCase):
+    pass
---
+++ testapp/testapp/testmain/views.py
@@ -0,0 +1,26 @@
+from django_renderpdf.views import PDFView
+
+
+class PromptDownloadView(PDFView):
+    template_name = 'test_template.html'
+    prompt_download = True
+    download_name = 'myfile.pdf'
+
+
+class NoPromptDownloadView(PDFView):
+    template_name = 'test_template.html'
+    prompt_download = False
+
+
+class AllowForceHtmlView(PDFView):
+    template_name = 'test_template.html'
+    allow_force_html = False
+
+
+class DisallowForceHtmlView(PDFView):
+    template_name = 'test_template.html'
+    allow_force_html = True
+
+
+class TemplateWithStaticFileView(PDFView):
+    template_name = 'test_template_with_staticfile.html'
