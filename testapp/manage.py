640cdd0563be3e6b10a43048a67864b78ddb5e0f
Hugo Osvaldo Barrera <hugo@barrera.io>
2021-08-02 22:18:19 +0200

    Update black formatting

--- testapp/manage.py
+++ testapp/manage.py
@@ -4,6 +4,6 @@ import sys
 
 from django.core.management import execute_from_command_line
 
-if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testapp.settings')
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "testapp.settings")
     execute_from_command_line(sys.argv)
