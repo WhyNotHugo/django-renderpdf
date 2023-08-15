1759353c6b1ead8f75ebfd2b7c61cdddfa76c84d
Hugo Osvaldo Barrera <hugo@whynothugo.nl>
2023-08-15 10:38:19 +0200

    Delete some commented-out code

--- docs/conf.py
+++ docs/conf.py
@@ -9,9 +9,6 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
 import django_renderpdf
 
 # -- Project information -----------------------------------------------------
